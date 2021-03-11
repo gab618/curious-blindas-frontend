99aea48caca8bddb0b7fb48a39a8d669b7d6f3e9
Gabriel <knuckles4x@gmail.com>
2021-03-10 22:08:48 -0300

    Register page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { AuthComponent } from './auth.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
-  declarations: [LoginComponent, AuthComponent],
+  declarations: [LoginComponent, AuthComponent, RegisterComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
