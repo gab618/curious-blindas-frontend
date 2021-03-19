de34322020fd030ff5bf684a348db925e7f41271
Gabriel <knuckles4x@gmail.com>
2021-03-19 19:56:56 -0300

    feat: add spinner and style to loading form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,9 +6,10 @@ import { LoginComponent } from './login/login.component';
 import { AuthComponent } from './auth.component';
 import { RegisterComponent } from './register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [LoginComponent, AuthComponent, RegisterComponent],
-  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, SharedModule],
 })
 export class AuthModule {}
---
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.scss'],
+})
+export class SpinnerComponent implements OnInit {
+  @Input() height = '24';
+  @Input() width = '24';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
