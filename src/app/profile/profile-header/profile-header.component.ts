74d9cc93be2ba406540e84bdb57bd886c4e7faee
Gabriel <knuckles4x@gmail.com>
2021-03-29 13:17:13 -0300

    feat: unauthenticated profile header

--- src/app/profile/profile-header/profile-header.component.ts
+++ src/app/profile/profile-header/profile-header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-header.component.scss'],
 })
 export class ProfileHeaderComponent implements OnInit {
-  isAuthenticated = true;
+  isAuthenticated = false;
 
   constructor() {}
 
