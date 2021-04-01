b3984d3026dcb3ca83fcf9be6bb4a193cc7d9bc3
Gabriel <knuckles4x@gmail.com>
2021-04-01 18:11:55 -0300

    feat: profile question component

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import { ProfileHeaderComponent } from './profile-header/profile-header.component';
+import { QuestionComponent } from './question/question.component';
 
 
 @NgModule({
-  declarations: [ProfileComponent, ProfileHeaderComponent],
+  declarations: [ProfileComponent, ProfileHeaderComponent, QuestionComponent],
   imports: [
     CommonModule,
     ProfileRoutingModule
